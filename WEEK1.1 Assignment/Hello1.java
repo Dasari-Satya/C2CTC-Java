cc167609e6506b0f51d234d7256f5695afb704d5
trinadh <trina@Trinadh-Pavilion>
2022-05-22 13:43:42 +0530

    Second Commit

---
+++ WEEK1.1 Assignment/Hello1.java
@@ -0,0 +1,17 @@
+//Code 8:Decision making with operators
+public class Hello1 {
+	public static void main(String[] args) 
+	{
+		int x = 5;
+          if(x != 5)
+          {
+        	  System.out.println("Value of x is not 5");
+        	  
+          }
+          else
+          {
+          System.out.println("Value of x is 5");
+          }
+	}
+
+}
---
+++ WEEK1.1 Assignment/Hello2.java
@@ -0,0 +1,18 @@
+//Code 9:Decision Making with operators
+public class Hello2 {
+	public static void main(String[] args) 
+	{
+		int x = 8, y = 7;
+		
+           if(x >= y)
+           {
+        	   System.out.println("true");
+           }
+           else
+           {
+        	   System.out.println("false");
+           }
+	}
+
+
+}
