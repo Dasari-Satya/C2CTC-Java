5c288bc94da129ea98b27586ba1c6b4132d095db
trinadh <trina@Trinadh-Pavilion>
2022-05-28 22:27:57 +0530

    First Commit

---
+++ Practice Programs/src/Variables.java
@@ -0,0 +1,17 @@
+
+public class Variables 
+{
+	public static void main(String args[])
+	{
+		int a=20;
+		float b=23;//Implicit conversion
+		float d=24.45f;
+		int c=(int)d;//Explicit Conversion(Type Conversion)
+		char e='A';
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(d);
+		System.out.println(c);
+	}
+
+}
