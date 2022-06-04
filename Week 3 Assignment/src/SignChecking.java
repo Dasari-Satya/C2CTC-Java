62307356e5447172db7fdafa72e107c17af4279a
trinadh <trina@Trinadh-Pavilion>
2022-06-04 20:41:04 +0530

    Third Comit

---
+++ Week 3 Assignment/src/SignChecking.java
@@ -0,0 +1,25 @@
+/*To get a number from the user and print whether 
+ it is positive or negative using scanner class*/
+import java.util.Scanner;
+public class SignChecking {
+	public static void main(String args[])
+	{
+		int num;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		num=sc.nextInt();
+		if(num>0)
+		{
+			System.out.println("The Number is Positive");
+		}
+		else if(num<0)
+		{
+			System.out.println("The Number is Negative");
+		}
+		else
+		{
+			System.out.println("The Number is Zero");
+		}
+	}
+
+}
