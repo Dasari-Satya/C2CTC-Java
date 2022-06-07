f26d0a07084177bdc4093ac241ac2979d706122a
trinadh <trina@Trinadh-Pavilion>
2022-06-07 11:43:53 +0530

    Fifth Commit

---
+++ DAY 14 Practice/Main1.java
@@ -0,0 +1,11 @@
+
+public class Main1 {
+	public static void main(String args[])
+	{
+		int num=10;
+		System.out.println("The Integer Value:"+num);
+		double data=num;
+		System.out.println("The Double Value:"+data);
+	}
+
+}
---
+++ DAY 14 Practice/NarrowTypeCasting.java
@@ -0,0 +1,11 @@
+
+public class NarrowTypeCasting {
+	public static void main(String args[])
+	{
+		double num=10.99;
+		System.out.println("The Double Value:"+num);
+		int data=(int)num;
+		System.out.println("The Integer Value:"+data);
+	}
+
+}
